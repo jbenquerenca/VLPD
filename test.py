3d2db6064d5643a9a9d63039b1d54da1fe0c6447
João Benquerença <37444053+jbenquerenca@users.noreply.github.com>
2024-12-03 20:13:25 +0000

    add tju to test.py

--- test.py
+++ test.py
@@ -38,7 +38,7 @@ def fix_seed(seed):
 
 def main():
     args = parse()
-    cfg = Config() if args.dataset == 'city' else ConfigCaltech()
+    cfg = Config() if args.dataset == 'tju' else ConfigCaltech()
 
     fix_seed(cfg.seed)
 
@@ -52,6 +52,10 @@ def main():
     # dataset
     print('Dataset...')
 
+    if args.dataset == 'tju':
+        testdataset = TJU(path=cfg.root_path, split='test', config=cfg)
+        testloader = DataLoader(testdataset, batch_size=1, num_workers=4)
+        val_tju(testloader, net, cfg, args)
     if args.dataset == 'city':
         testdataset = CityPersons(path=cfg.root_path, type='val', config=cfg)
         testloader = DataLoader(testdataset, batch_size=1, num_workers=4)
@@ -159,6 +163,52 @@ def val_caltech(testloader: DataLoader, net, config: ConfigCaltech, args):
 
     FPS = int(num_imgs / (t4 - t3))
     print('FPS : {}'.format(FPS))
+
+def val_tju(testloader, net, config, args):
+    net.eval()
+
+    print('Perform validation...')
+    temp_val = args.out_path
+    is_inference = not os.path.exists(temp_val)
+
+    if is_inference:
+        res = []
+        inference_time = 0
+        num_images = len(testloader)
+        for i, data in enumerate(testloader):
+            inputs = data.cuda()
+            with torch.no_grad():
+                t1 = time.time()
+                results = net(inputs, is_train=False)
+                pos, height, offset = results[:3]
+                t2 = time.time()
+                inference_time += (t2 - t1)
+
+            boxes = parse_det_offset(pos.cpu().numpy(), height.cpu().numpy(), offset.cpu().numpy(), 
+                                config.size_test, score=0.1, down=4, nms_thresh=0.5)
+            if len(boxes) > 0:
+                boxes[:, [2, 3]] -= boxes[:, [0, 1]]
+                for box in boxes:
+                    temp = dict()
+                    temp['image_id'] = i+1
+                    temp['category_id'] = 1
+                    temp['bbox'] = box[:4].tolist()
+                    temp['score'] = float(box[4])
+                    res.append(temp)
+
+            print('\r%d/%d' % (i + 1, num_images),end='')
+            sys.stdout.flush()
+
+        with open(temp_val, 'w') as f:
+            json.dump(res, f)
+
+    MRs = validate('data/TJU-DHD-Traffic/annotations/test.json', temp_val)
+    print('\nSummerize:[Reasonable: %.2f%%], [Reasonable_small: %.2f%%], [Reasonable_occ=heavy: %.2f%%], [All: %.2f%%]'
+          % (MRs[0]*100, MRs[1]*100, MRs[2]*100, MRs[3]*100))
+    if is_inference:
+        FPS = int(num_images / inference_time)
+        print('FPS : {}'.format(FPS))
+
 
 if __name__ == '__main__':
     main()
