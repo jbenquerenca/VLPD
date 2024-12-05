f8904e30b4c705b5872fc9df7ad49e590a68f291
João Benquerença <37444053+jbenquerenca@users.noreply.github.com>
2024-12-05 19:58:42 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-pip install opencv-python scipy matplotlib ftfy regex tqdm
+pip install opencv-python scipy matplotlib ftfy regex tqdm Cython
 pip install git+https://github.com/openai/CLIP.git
 pip install "numpy<1.24"
 wget https://openaipublic.azureedge.net/clip/models/afeb0e10f9e5a86da6080e35cf09123aca3b358a0c3e3b6c78a7b63bc04b6762/RN50.pt
